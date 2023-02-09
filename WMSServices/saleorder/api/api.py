2fc5bb9249e15b8ee5a57242155ed72bd73a9b61
Sebastian Narvaez Lopera <senl5248@coomeva.com.co>
2023-02-08 23:49:31 -0500

    Configuracion ViewSet para app sale order

--- WMSServices/saleorder/api/api.py
+++ WMSServices/saleorder/api/api.py
@@ -1,15 +1,11 @@
 # restframework
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 # local apps
 from saleorder.models import SaleOrder
 from saleorder.api.serializers import SaleOrderSerializer
 
-class SaleOrderAPIView(APIView):
-    """APIView class SaleOrderAPIView"""
-
-    def get(self, request):
-        saleorder = SaleOrder.objects.all()
-        saleorder_serializer = SaleOrderSerializer(saleorder, many=True)
-        return Response(saleorder_serializer.data)
+class SaleOrderViewSet(viewsets.ModelViewSet):
+    """View Set class SaleOrderAPIView"""
+    queryset = SaleOrder.objects.all()
+    serializer_class = SaleOrderSerializer
--- WMSServices/saleorder/api/urls.py
+++ WMSServices/saleorder/api/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
-from saleorder.api.api import SaleOrderAPIView
+#Django
+from django.urls import path, include
+
+# DRF
+from rest_framework import routers
+
+# LocalApps
+from saleorder.api.api import SaleOrderViewSet
+
+router = routers.DefaultRouter()
+router.register(r'', SaleOrderViewSet)
 
 urlpatterns = [
-    path('', SaleOrderAPIView.as_view(), name='saleorder_api'),
-    #path('get/<str:num_sale_order>/', views.get_sale_order),
-    #path('getitems/<str:num_sale_order>/', views.get_sale_order_items),
-    #path('getinfoindicators/<str:name_customer>/<str:no_sale_order>/', views.get_info_indicators),
+    path('', include(router.urls)),
 ]
