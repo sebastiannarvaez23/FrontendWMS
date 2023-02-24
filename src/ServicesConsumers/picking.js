de65c14e7edabb759ed8e6abb9ca20b47669643d
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2023-02-24 00:31:30 -0500

    Implementando header para que envie token de autorizacion en las peticiones al core picking

--- src/ServicesConsumers/picking.js
+++ src/ServicesConsumers/picking.js
@@ -3,7 +3,11 @@ import { API } from './api/base.js';
 
 export const getPickings = async (setPickings, setLoadedPicking, noSaleOrder) => {
     if (noSaleOrder !== "") {
-        await axios.get(API + "picking/" + noSaleOrder)
+        await axios.get(API + "picking/" + noSaleOrder, {
+            headers: {
+                "Authorization": "Token 65e4067fb71f7f60c5311862cc4ecc41788e2713"
+            }
+        })
             .then(response => {
                 setPickings(response.data);
             })
@@ -15,16 +19,26 @@ export const getPickings = async (setPickings, setLoadedPicking, noSaleOrder) =>
 }
 
 export const createPicking = async (data, setPickings, setLoadedPicking, noSaleOrder) => {
-    await axios.post(API + "picking/", data)
-        .then((response) => {
-            //const { data } = response;
+    if (noSaleOrder !== "") {
+        await axios.post(API + "picking/", data, {
+            headers: {
+                "Authorization": "Token 65e4067fb71f7f60c5311862cc4ecc41788e2713"
+            }
         })
-        .catch(err => console.log(err))
-        .finally(() => getPickings(setPickings, setLoadedPicking, noSaleOrder))
+            .then((response) => {
+                //const { data } = response;
+            })
+            .catch(err => console.log(err))
+            .finally(() => getPickings(setPickings, setLoadedPicking, noSaleOrder))
+    }
 }
 
 export const deletePicking = async (idPicking, setPickings, setLoadedPicking, noSaleOrder) => {
-    await axios.delete(API + "picking/" + idPicking)
+    await axios.delete(API + "picking/" + idPicking, {
+        headers: {
+            "Authorization": "Token 65e4067fb71f7f60c5311862cc4ecc41788e2713"
+        }
+    })
         .then((response) => {
             //const { data } = response;
         })
