904c2debc33db478d1708c36516f93bcc39da466
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2023-02-27 22:59:49 -0500

    Iniciando las interacciones con las alertas

--- src/ServicesConsumers/auth.js
+++ src/ServicesConsumers/auth.js
@@ -8,11 +8,12 @@ const https = require('https');
 export const loginUser = async (username, password, setUser, navigate) => {
   await axios.post(API + "registration/users/login/",  { "username": username, "password": password }, confRequestLogin)
       .then((response) => {
-        //const { data } = response;
         if (response.data.status === 200) {
           Cookies.set('token', response.data.access_token);
           setUser(response.data.user)
           navigate("/picking");
+        } else {
+          
         }
       })
       .catch(err => console.log(err))
