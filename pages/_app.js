086372d1857f2db79ced0143a48802364ac54756
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2023-04-02 21:49:11 -0500

    Estabilizando el contexto de autenticacion; sin embargo se integrará Redux para dejar de lado el context

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../public/styles.css';
+import { AuthProvider } from "../src/Context/auth-context";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
