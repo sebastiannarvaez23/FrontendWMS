1e6a5766050a5e29ba7f6d02938365f1138ca40d
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2023-05-30 13:05:32 -0500

    Incluyendo archivos de configuración del entorno y el modo de desarrollo

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    reactStrictMode: true,
+    env: {
+        customKey: 'customValue',
+    },
+    basePath: '/dist',
+    compress: true,
+    async redirects() {
+        return [{
+            source: '/hola',
+            destination: '/hello',
+            permanent: true
+        }]
+    }
+}
